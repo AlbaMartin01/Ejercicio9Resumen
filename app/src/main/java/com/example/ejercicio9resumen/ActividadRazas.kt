6b79eda0a26a08334d36969d994fccd5430d9de9
AlbaMartin01 <albmar891@alu.centroafuera.es>
2023-01-26 12:43:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio9resumen/ActividadRazas.kt
@@ -0,0 +1,44 @@
+package com.example.ejercicio9resumen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class ActividadRazas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad_razas)
+
+        var stringRazas = ""
+        var clase = intent.getStringExtra("imagenC")
+
+        findViewById<Button>(R.id.botonElfo).setOnClickListener{
+            findViewById<ImageView>(R.id.imagenInicio).setImageResource(R.drawable.elfo)
+            stringRazas = "elfo"
+        }
+
+        findViewById<Button>(R.id.botonHumano).setOnClickListener{
+            findViewById<ImageView>(R.id.imagenInicio).setImageResource(R.drawable.humano)
+            stringRazas = "humano"
+        }
+
+        findViewById<Button>(R.id.botonEnano).setOnClickListener{
+            findViewById<ImageView>(R.id.imagenInicio).setImageResource(R.drawable.enano)
+            stringRazas = "enano"
+        }
+
+        findViewById<Button>(R.id.botonGoblin).setOnClickListener{
+            findViewById<ImageView>(R.id.imagenInicio).setImageResource(R.drawable.goblin)
+            stringRazas = "goblin"
+        }
+
+        findViewById<Button>(R.id.botonAceptar).setOnClickListener{
+            val intento = Intent(this,MainActivity::class.java)
+            intento.putExtra("imagenC", clase)
+            intento.putExtra("imagenR", stringRazas)
+            startActivity(intento)
+        }
+    }
+}
